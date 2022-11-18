f549435b0011c0c4632adfe072097ea0ba114c86
arcaneJesus <elijahz0409@gmail.com>
2022-11-18 05:42:47 -0500

    Added Weapon struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,42 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+pub mod Inv {
+    pub mod Item {
+
+        enum Pattern {
+            Melee,
+            Ranged,
+            Defensive,
+        }
+
+        struct Attack {
+            damage: i32,
+            speed: i32,
+            penetration: i32,
+            description: String,
+        }
+
+        impl Attack {
+            fn calc(&self, armor: Armor) -> HitOrMiss {}
+        }
+
+        enum HitOrMiss {
+            // Yah I bet you never miss ha.  You got a boyfriend? I bet he doesn't kiss ya.
+            Hit(i32),
+            Miss,
+        }
+
+        struct Weapon {
+            primary: Attack,
+            secondary: Attack,
+            tertiary: Attack,
+            pattern: Pattern,
+            description: String,
+        }
+
+        struct Armor {}
     }
+}
+
+#[cfg(test)]
+mod Test {
+    use super::*;
 }
